eca6b49975295a5b3d77eb0544fd5aad07767fc8
SubmarineAndGreen <scythed66@gmail.com>
2019-09-18 14:12:45 +0200

    removed redundant using statements, removed extra hosting port

--- surveys-api/Data/ViewModels/AnswerViewModel.cs
+++ surveys-api/Data/ViewModels/AnswerViewModel.cs
@@ -1,38 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.ComponentModel;
-
 
 namespace aprototype.ViewModels
 {
     [JsonObject(MemberSerialization.OptOut)]
-
-    public class SurveyViewModel
+    public class AnswerViewModel
     {
-        public SurveyViewModel()
+        public AnswerViewModel()
         {
 
         }
 
         #region properties
         public int ID { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public int SurveyID { get; set; }
+        public int QuestionID { get; set; }
         public string Text { get; set; }
         public string Notes { get; set; }
         [DefaultValue(0)]
         public int Type { get; set; }
         [DefaultValue(0)]
         public int Flags { get; set; }
-        public string UserID { get; set; }
+        [DefaultValue(0)]
+        public int Value { get; set; }
         [JsonIgnore]
-        public int ViewCount { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime LastModifiedDate { get; set; }
         #endregion
+
+
+
 
     }
 }
--- surveys-api/Data/ViewModels/SurveyViewModel.cs
+++ surveys-api/Data/ViewModels/SurveyViewModel.cs
@@ -1,39 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.ComponentModel;
+
 
 namespace aprototype.ViewModels
 {
     [JsonObject(MemberSerialization.OptOut)]
-    public class AnswerViewModel
+
+    public class SurveyViewModel
     {
-        public AnswerViewModel()
+        public SurveyViewModel()
         {
 
         }
 
         #region properties
         public int ID { get; set; }
-        public int SurveyID { get; set; }
-        public int QuestionID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
         public string Text { get; set; }
         public string Notes { get; set; }
         [DefaultValue(0)]
         public int Type { get; set; }
         [DefaultValue(0)]
         public int Flags { get; set; }
-        [DefaultValue(0)]
-        public int Value { get; set; }
+        public string UserID { get; set; }
         [JsonIgnore]
+        public int ViewCount { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime LastModifiedDate { get; set; }
         #endregion
-
-
-
 
     }
 }
